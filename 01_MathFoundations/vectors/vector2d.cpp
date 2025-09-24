f444d2bfe770d07f3f75fcb08d006014dcd79ee1
@specktro <specktro@nonull.mx>
2025-09-23 21:46:00 -0600

    feat: Add some functions to Vector2D class.

--- 01_MathFoundations/vectors/vector2d.cpp
+++ 01_MathFoundations/vectors/vector2d.cpp
@@ -23,3 +23,41 @@ float Vector2D::length() const {
 float Vector2D::dot(const Vector2D& other) const {
     return x * other.x + y * other.y;
 }
+
+Vector2D Vector2D::normalize() const {
+    float len = length();
+
+    if (len == 0) {
+        return {0, 0};  // Avoid division by zero
+    }
+
+    return {x / len, y / len};
+}
+
+float Vector2D::distanceTo(const Vector2D& other) const {
+    return std::sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
+}
+
+Vector2D Vector2D::perpendicular() const {
+    return {y, -x};
+}
+
+// TODO: Implement the following method
+float Vector2D::angleBetween(const Vector2D& other) const {
+    return 0.0f;
+}
+
+Vector2D Vector2D::scale(float factor) const {
+    return {x * factor, y * factor};
+}
+
+// Static utility methods
+// TODO: Implement the following method
+static Vector2D lerp(const Vector2D& a, const Vector2D& b, float t) {
+    return {0, 0};
+}
+
+// TODO: Implement the following method
+static Vector2D fromAngle(float angleRadians) {
+    return {0, 0};
+}
--- 01_MathFoundations/vectors/vector2d.h
+++ 01_MathFoundations/vectors/vector2d.h
@@ -8,11 +8,20 @@ private:
 public:
     Vector2D(float x = 0.0f, float y = 0.0f);
 
-    // TODO: We'll implement these step by step
     Vector2D operator+(const Vector2D& other) const;
     Vector2D operator-(const Vector2D& other) const;
     float length() const;
     float dot(const Vector2D& other) const;
+
+    Vector2D normalize() const;           // Unit vector (length = 1)
+    float distanceTo(const Vector2D& other) const;  // Distance between points
+    Vector2D perpendicular() const;       // 90-degree rotation
+    float angleBetween(const Vector2D& other) const;  // Angle in radians
+    Vector2D scale(float factor) const;   // Multiply by scalar
+
+    // Static utility methods
+    static Vector2D lerp(const Vector2D& a, const Vector2D& b, float t);  // Linear interpolation
+    static Vector2D fromAngle(float angleRadians);  // Create vector from angle
 
     // Getters for testing
     float getX() const { return x; }
